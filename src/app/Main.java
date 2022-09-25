61a113d13a6eb9e1e7ea7de8736e40b27ad5f1ee
Li4nostMT <maksimtsaran1@gmail.com>
2022-09-25 16:19:17 +0300

    Add method variable initialization and handle data

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,19 +2,14 @@ package app;
 
 public class Main {
 
+    static int a;
+    static int b;
+
     public static void main(String[] args) {
         System.out.println("Calculator for base calculations");
         System.out.println("Version 1.0");
-        int a = 8;
-        int b = 2;
-        int c = add(a, b);
-        System.out.println(c);
-        int m = multiply(a, b);
-        System.out.println(m);
-        int s = subtraction(a, b);
-        System.out.println(s);
-        int d = division(a, b);
-        System.out.println(d);
+        variableInfo();
+        handleData();
     }
 
     private static int add(int a, int b) {
@@ -31,5 +26,21 @@ public class Main {
 
     private static int division(int a, int b) {
         return a / b;
+    }
+
+    private static void variableInfo() {
+        a = 8;
+        b = 2;
+    }
+
+    private static void handleData() {
+        int c = add(a, b);
+        System.out.println(c);
+        int m = multiply(a, b);
+        System.out.println(m);
+        int s = subtraction(a, b);
+        System.out.println(s);
+        int d = division(a, b);
+        System.out.println(d);
     }
 }
